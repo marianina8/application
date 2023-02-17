f0ee60fac7a4f37f4c7bc23fdbef2e2734be13d6
Marian Montagnino <mmontagnino@netflix.com>
2023-02-16 23:36:44 -0800

    Chapter-5 cleanup

--- cmd/tty.go
+++ cmd/tty.go
@@ -24,14 +24,4 @@ var ttyCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(ttyCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// ttyCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// ttyCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
